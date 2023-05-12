4a346902110649e424dcc7b5123fce226084fe8c
chrisgg <chris@greyson-gaito.co.uk>
2023-05-11 20:04:48 -0400

    cleaned stability script

--- scripts/bacteriophage_stability.jl
+++ scripts/bacteriophage_stability.jl
@@ -13,13 +13,6 @@ function optimum(selectiondata)
     return optimumdata
 end
 
-# function attractordata(selectiondata, par)
-#     attractor = zeros(length(selectiondata))
-#     for i in 1:length(selectiondata)
-#         attractor[i] = stableequil(selectiondata[i], par)
-#     end
-#     return attractor
-# end 
 
 function setupbparam(brange)
     for bi in 1:length(brange)
@@ -34,58 +27,6 @@ function trackoptimum(solutiondata, optimumdata)
     end
     return optimumdiff
 end
-
-let
-    u0=[0.5]
-    tsend = 10000
-    freq = 0.1
-    tspan=(0.0, tsend)
-    par = BacPhageSineForcedPar(b = 0.01, r=0.01, per=0.5, amp=0.4, mid=-0.01)
-    prob = ODEProblem(bacphage_sine_forced!, u0, tspan, par)
-    sol = solve(prob, RadauIIA5())
-    solseries = sol(tsend-100.0:freq:tsend)
-    selectiondata = [sel_sine(par, t) for t in tsend-100.0:freq:tsend]
-    seriesoptimum = optimum(selectiondata)
-    optimumdiff = trackoptimum(solseries[1,:], seriesoptimum)
-    # test = figure()
-    # plot(tsend-100.0:freq:tsend, optimumdiff)
-    # # plot(tsend-100.0:freq:tsend, solseries[1,:])
-    # plot(tsend-100.0:freq:tsend, seriesoptimum)
-    # return test
-    return mean(optimumdiff)
-end
-
-# function trackattractor_INT(solutiondata, optimumdata)
-#     INTdiff = zeros(length(solutiondata))
-#     for i in 1:length(solutiondata)
-#         INTdiff[i] = solutiondata[i] - optimumdata[i]
-#     end
-#     return log10(abs(sum(INTdiff)))
-# end
-
-
-
-# function trackattractor_INT3(solutiondata, optimumdata)
-#     return log10(abs(sum(solutiondata)-sum(optimumdata)))
-# end
-
-# function trackingcor_INT_b(brange, rval, freq, tsend)
-#     data = zeros(length(brange), 2)
-#     u0=[0.5]
-#     tspan=(0.0, tsend)
-#     @threads for bi in eachindex(brange)
-#         par = BacPhageSineForcedPar(b = brange[bi], r=rval, per=0.5, amp=0.4, mid=-0.002)
-#         prob = ODEProblem(bacphage_sine_forced!, u0, tspan, par)
-#         sol = solve(prob, RadauIIA5())
-#         solseries = sol(tsend-1000.0:freq:tsend)
-#         selection = [sel_sine(par, t) for t in tsend-1000.0:freq:tsend]
-#         seriesattractor = attractordata(selection, par)
-#         INT = trackattractor_INT(solseries[1,:], seriesattractor)
-#         data[bi, 1] = brange[bi]
-#         data[bi, 2] = INT
-#     end
-#     return data
-# end
 
 function brconstrained_stabilitytracking(bplusrrange, brratio, smid, freq, tsend)
     data = zeros(length(bplusrrange), 2)
