9c14875e5af3415f17b7339856938124c9dcda2d
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2024-02-16 14:29:49 -0500

    saved supporting information again

--- scripts/bacteriophage_supportinginformation.jl
+++ scripts/bacteriophage_supportinginformation.jl
@@ -18,18 +18,18 @@ function range_parameter_br(brratio, bplusrrange)
     return hcat(minb, maxb, minr, maxr)
 end
 
-range_parameter_br(0.1, 0.00001:0.0001:0.003)
-range_parameter_br(1.0, 0.00001:0.0001:0.003)
-range_parameter_br(10.0, 0.00001:0.0001:0.003)
+range_parameter_br(0.1, 0.00001:0.0001:0.001)
+range_parameter_br(1.0, 0.00001:0.0001:0.001)
+range_parameter_br(10.0, 0.00001:0.0001:0.001)
 
-0.4*sin(π/2)-0.002
--0.4*sin(π/2)-0.002
+0.4*sin(π/2)-0.0005
+-0.4*sin(π/2)-0.0005
 
 function sel_noise_range(reps)
     mins = zeros(reps)
     maxs = zeros(reps)
     @threads for i in 1:reps
-        noisedata = noise_creation(-0.002, 0.05, 0.0, 1000, i)
+        noisedata = noise_creation(-0.0005, 0.05, 0.0, 1000, i)
         mins[i] = minimum(noisedata)
         maxs[i] = maximum(noisedata)
     end
